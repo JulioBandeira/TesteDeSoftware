b615480b4e5bd9d1204fc82cd19e8bd70b889180
Julio cesar <info.bandeira@gmail.com>
2021-05-04 21:04:49 -0400

    add metodo Theory Calculadora_Retorna_ValorSomaCorretos

--- 1.1 Teste Basicos/Demo.Teste/CalculadoraTeste.cs
+++ 1.1 Teste Basicos/Demo.Teste/CalculadoraTeste.cs
@@ -17,5 +17,21 @@ namespace Demo.Teste
             //assert
             Assert.Equal(4, resultado);
         }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        [InlineData(6,7,13)]     
+        public void Calculadora_Retorna_ValorSomaCorretos(int v1, int v2, int total)
+        {
+            //arrange
+            var calculadora = new Calculadora();
+
+            //act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //assert
+            Assert.Equal(total, resultado);
+        }
     }
 }
